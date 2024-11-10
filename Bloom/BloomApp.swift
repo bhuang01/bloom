7def907c78f584020437b9678e6fdd483b8f67ff
Bryan Huang <b_huang@cs.ucsb.edu>
2024-11-09 23:07:28 -0800

    Initial Commit

---
+++ Bloom/BloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomApp.swift
+//  Bloom
+//
+//  Created by Bryan Huang on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BloomTests/BloomTests.swift
@@ -0,0 +1,17 @@
+//
+//  BloomTests.swift
+//  BloomTests
+//
+//  Created by Bryan Huang on 11/9/24.
+//
+
+import Testing
+@testable import Bloom
+
+struct BloomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
